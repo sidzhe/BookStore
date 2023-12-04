d855283177309546dba719e203bc34fae7a783ae
Fedor Igonin <murzik.212@yandex.ru>
2023-12-04 23:44:42 +0400

    add elements on view

--- BookStore/AccountScreen/AccountViewController.swift
+++ BookStore/AccountScreen/AccountViewController.swift
@@ -1,13 +1,11 @@
-//
-//  AccountViewController.swift
-//  BookStore
-//
-//  Created by sidzhe on 03.12.2023.
-//
+
 
 import UIKit
+import SnapKit
 
-final class AccountViewController: UIViewController {
+class AccountViewController: UIViewController {
+    
+    let nameTextField = UITextField()
     
     //MARK: - Presenter
     var presenter: AccountPresenterProtocol!
@@ -16,13 +14,67 @@ final class AccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .purple
+        view.backgroundColor = .white
+        
+        initialize()
+    }
+    
+    private func initialize(){
+        
+        let label = UILabel()
+        label.text = "Acount"
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(100)
+            make.centerX.equalToSuperview()
+        }
+        
+        let avatar = UIView()
+        avatar.backgroundColor = .black
+        avatar.layer.cornerRadius = 50
+        view.addSubview(avatar)
+        avatar.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(label.snp.bottom).offset(75)
+            make.width.equalTo(150)
+            make.height.equalTo(150)
+        }
+        
+        //let nameTextField = UITextField()
+        nameTextField.backgroundColor = .purple
+        view.addSubview(nameTextField)
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(avatar.snp.bottom).offset(50)
+            make.centerX.equalToSuperview()
+            make.left.equalTo(20)
+            make.right.equalTo(20)
+        }
+        
+        
+        
         
     }
 }
+    //MARK: - AccountViewProtocol
+    extension AccountViewController: AccountViewProtocol {
+       
+    }
 
 
-//MARK: - AccountViewProtocol
-extension AccountViewController: AccountViewProtocol {
+//MARK: - UITextFieldDelegate
+
+extension AccountViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.endEditing(true)
+        return true
+    }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if nameTextField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Your name"
+            return false
+        }
+    }
 }
